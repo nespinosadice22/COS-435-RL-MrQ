aab1f401aa16944c537e39c686585c82719c0781
Natalia Espinosa Dice <ne3496@princeton.edu>
2025-04-07 15:18:50 -0400

    reworking agent

---
+++ mrq/hyperparams.py
@@ -0,0 +1,63 @@
+import dataclasses 
+
+#at some point we can rename these - or find a diff library to do this! 
+@dataclasses.dataclass
+class Hyperparameters:
+    # Generic
+    batch_size: int = 256
+    buffer_size: int = 1e6
+    discount: float = 0.99
+    target_update_freq: int = 250
+
+    # Exploration
+    buffer_size_before_training: int = 10e3
+    exploration_noise: float = 0.2
+
+    # TD3
+    target_policy_noise: float = 0.2
+    noise_clip: float = 0.3
+
+    # Encoder Loss
+    dyn_weight: float = 1
+    reward_weight: float = 0.1
+    done_weight: float = 0.1
+
+    # Replay Buffer (LAP)
+    prioritized: bool = True
+    alpha: float = 0.4
+    min_priority: float = 1
+    enc_horizon: int = 5
+    Q_horizon: int = 3
+
+    # Encoder Model
+    zs_dim: int = 512
+    zsa_dim: int = 512
+    za_dim: int = 256
+    enc_hdim: int = 512
+    enc_activ: str = 'elu'
+    enc_lr: float = 1e-4
+    enc_wd: float = 1e-4
+    pixel_augs: bool = True
+
+    # Value Model
+    value_hdim: int = 512
+    value_activ: str = 'elu'
+    value_lr: float = 3e-4
+    value_wd: float = 1e-4
+    value_grad_clip: float = 20
+
+    # Policy Model
+    policy_hdim: int = 512
+    policy_activ: str = 'relu'
+    policy_lr: float = 3e-4
+    policy_wd: float = 1e-4
+    gumbel_tau: float = 10
+    pre_activ_weight: float = 1e-5
+
+    # Reward model
+    num_bins: int = 65
+    lower: float = -10
+    upper: float = 10
+
+    #idk if we need
+    #def __post_init__(self): utils.enforce_dataclass_type(self)
--- mrq/utils.py
+++ mrq/utils.py
@@ -1,3 +1,54 @@
 '''
-any utils needed
+currently the same - we should change!
 '''
+# utils.py
+import torch
+import torch.nn.functional as F
+
+
+def realign(x, discrete: bool):
+    return F.one_hot(x.argmax(1), x.shape[1]).float() if discrete else x.clamp(-1,1)
+
+
+def masked_mse(x: torch.Tensor, y: torch.Tensor, mask: torch.Tensor):
+    return (F.mse_loss(x, y, reduction='none') * mask).mean()
+
+
+def multi_step_reward(reward: torch.Tensor, not_done: torch.Tensor, gammas: torch.Tensor):
+    return (reward * not_done * gammas).sum(1), not_done.prod(1)
+
+
+def maybe_augment_state(state: torch.Tensor, next_state: torch.Tensor, pixel_obs: bool, use_augs: bool):
+    if pixel_obs and use_augs:
+        if len(state.shape) != 5: state = state.unsqueeze(1)
+        batch_size, horizon, history, height, width = state.shape
+
+        # Group states before augmenting.
+        both_state = torch.concatenate([state.reshape(-1, history, height, width), next_state.reshape(-1, history, height, width)], 0)
+        both_state = shift_aug(both_state)
+
+        state, next_state = torch.chunk(both_state, 2, 0)
+        state = state.reshape(batch_size, horizon, history, height, width)
+        next_state = next_state.reshape(batch_size, horizon, history, height, width)
+
+        if horizon == 1:
+            state = state.squeeze(1)
+            next_state = next_state.squeeze(1)
+    return state, next_state
+
+
+# Random shift.
+def shift_aug(image: torch.Tensor, pad: int=4):
+    batch_size, _, height, width = image.size()
+    image = F.pad(image, (pad, pad, pad, pad), 'replicate')
+    eps = 1.0 / (height + 2 * pad)
+
+    arange = torch.linspace(-1.0 + eps, 1.0 - eps, height + 2 * pad, device=image.device, dtype=torch.float)[:height]
+    arange = arange.unsqueeze(0).repeat(height, 1).unsqueeze(2)
+
+    base_grid = torch.cat([arange, arange.transpose(1, 0)], dim=2)
+    base_grid = base_grid.unsqueeze(0).repeat(batch_size, 1, 1, 1)
+
+    shift = torch.randint(0, 2 * pad + 1, size=(batch_size, 1, 1, 2), device=image.device, dtype=torch.float)
+    shift *= 2.0 / (height + 2 * pad)
+    return F.grid_sample(image, base_grid + shift, padding_mode='zeros', align_corners=False)
