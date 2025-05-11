539bbae76ec3b16d386ec17f79aa6a01b28e96bf
Natalia Espinosa Dice <ne3496@princeton.edu>
2025-05-11 14:11:29 -0400

    two hot with citations

--- mrq/two_hot.py
+++ mrq/two_hot.py
@@ -1,6 +1,5 @@
-# two_hot.py--> MRQ TwoHot reward‐quantization utility
-# Source: FacebookResearch/MRQ MRQ.py (class TwoHot)
-# Licensed under CC BY-NC 4.0
+# two_hot.py--> MRQ TwoHot reward encoding 
+# @citation: two_hot.py adapted from https://github.com/facebookresearch/MRQ/tree/main.
 """
 Provides a “two‐hot” encoding of scalar rewards into a categorical distribution
 over `num_bins` bins using symexp.
@@ -13,30 +12,15 @@ Includes methods for:
 import torch
 import torch.nn.functional as F
 
-
 class TwoHot:
-    """
-    Convert scalar reward --> two adjacent bins for classification
-    Cross-entropy loss
-    """
-
-    def __init__(
-        self,
-        device: str = "cuda",
-        lower_bound: float = -10.0,
-        upper_bound: float = 10.0,
-        num_bins: int = 65,
-    ):
+    def __init__(self, device: str = "cuda", lower_bound: float = -10.0, upper_bound: float = 10.0, num_bins: int = 65):
         self.num_bins = num_bins
         self.device = device
         self.bins = torch.linspace(lower_bound, upper_bound, num_bins, device=device)
         self.bins = self.bins.sign() * (self.bins.abs().exp() - 1)  # symexp
 
+    # Converts scalar rewards to two hot. Returns (batch_size, num_bins) distribution
     def two_hot_encode(self, scalar_rewards: torch.Tensor) -> torch.Tensor:
-        """
-        Converts scalar rewards to two hot. Returns (batch_size, num_bins) distribution
-        This is their function rn
-        """
         diff = scalar_rewards - self.bins.reshape(1, -1)
         diff = diff - 1e8 * (torch.sign(diff) - 1)
         ind = torch.argmin(diff, 1, keepdim=True)
@@ -52,9 +36,11 @@ class TwoHot:
         two_hot.scatter_(1, (ind + 1).clamp(0, self.num_bins), weight)
         return two_hot
 
+    #undo 
     def inverse(self, x: torch.Tensor):
         return (F.softmax(x, dim=-1) * self.bins).sum(-1, keepdim=True)
-
+        
+    #cross entropy loss 
     def cross_entropy_loss(self, pred: torch.Tensor, target: torch.Tensor):
         pred = F.log_softmax(pred, dim=-1)
         target = self.two_hot_encode(target)
